da4a88685529303e02cd34980fba9c3ca974a6c2
unknown <abdulkarimansari2000@gmail.com>
2022-07-25 06:52:26 +0530

    cleaning the code

--- src/app/dashbord/dashbord/dashbord.component.ts
+++ src/app/dashbord/dashbord/dashbord.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
 import { ApiService } from '../api.service';
-import {dummy} from './jsonDummyData'
+import { dummy } from './jsonDummyData';
+import { currentConditions } from './weatherdatainterface';
 
 @Component({
   selector: 'app-dashbord',
@@ -10,56 +11,24 @@ import {dummy} from './jsonDummyData'
 })
 export class DashbordComponent implements OnInit {
   constructor(private api: ApiService) {}
-  search(city:string){
-    this.tempAndTime=[];
-    this.api.getWeather(city).subscribe(value=>{this.json(value);
-      this.hourlyWeather(value)
-    
-    }
-
-    );
-    // this.json(jsonResponse);
-
+  search(city: string) {
+    this.tempAndTime = [];
+    this.api.getWeather(city).subscribe((value) => {
+      this.json(value);
+      this.hourlyWeather(value);
+    });
   }
 
-  address: string = '';
-  sunrise:any=null;
-  sunset:any=null;
   date = new Date();
-  condition:string=''
-  hoursToDisplay=0
-  temp: number = 0;
-  maxtemp: number = 0;
-  mintemp: number = 0;
-  humidity: number = 0;
-  visibility: number = 0;
-  wind: number = 0;
-  pressure: number = 0;
-  feelslike: number = 0;
-  cloud: number = 0;
-  description: string = '';
-  houre:any = 0;
+  condition: string = '';
+  houre: any = 0;
   minute: any = 0;
-  ampm: string = '';
   day: number = 0;
   month: number = 0;
   year: number = 0;
-  icon: Subject<any> = new Subject();
-
-  // address:string='';
-  // address:string='';
-  // address:string='';
+  currentWeatherData: any = {};
   ngOnInit(): void {
-    this.imgurls().subscribe(
-      (data) => (this.imgurl = `../../../assets/logos/${data}.png`)
-    );
     this.json(dummy.data);
-    this.hourlyWeather(dummy.data)
-    // this.api.getWeather('bundi').subscribe(value=>{
-    //   this.json(value);
-    //   this.hourlyWeather(value);
-    // })
-    
 
     setInterval(() => {
       let date = new Date();
@@ -67,88 +36,84 @@ export class DashbordComponent implements OnInit {
       this.month = 1 + date.getMonth();
       this.year = date.getFullYear();
       let tempminute = date.getMinutes();
-      this.minute =0;
-      if(tempminute<10){
-        this.minute=`0${tempminute}`
-      }
-      else{
-        this.minute=tempminute;
+      this.minute = 0;
+      if (tempminute < 10) {
+        this.minute = `0${tempminute}`;
+      } else {
+        this.minute = tempminute;
       }
     });
   }
-  json(data: any) {
-    let str: string = data.currentConditions.datetime;
-    console.log(str)
-    this.houre=parseInt(str.slice(0,2));
-    if(this.houre>12){
-      this.hoursToDisplay=this.houre-12;
-    this.ampm='PM'}
-      else{
-        this.hoursToDisplay=this.houre;
-        this.ampm='AM'
-      }
-      this.sunrise=data.currentConditions.sunrise;
-      this.sunset=data.currentConditions.sunset;
-      this.condition = data.currentConditions.conditions;
-    this.address = data.resolvedAddress;
-    this.temp = data.currentConditions.temp;
-    this.maxtemp = data.days[0].tempmax;
-    this.mintemp = data.days[0].tempmin;
-    this.wind = data.currentConditions.windspeed;
-    this.humidity = data.currentConditions.humidity;
-    this.pressure = data.currentConditions.pressure;
-    this.visibility = data.currentConditions.visibility;
-    this.cloud = data.currentConditions.cloudcover;
-    this.feelslike = data.currentConditions.feelslike;
-    this.icon.next(data.currentConditions.icon);
-    this.description = data.days[0].description;
-  }
-  imgurl: any;
-  imgurls(): Observable<any> {
-    return this.icon;
-  }
-  tempAndTime:Array<any> = [];
-  hourlyWeather = (data:any) => {
-    return new Promise((res,rej) => {
-      res(()=>{})
-      let j=1;
-      let l=0
-      let k=3;
-      let forcastTime:any=0;
-      let json=data.days;
-      let houre=this.houre;
-      let time=houre+k;
-      
 
-     
-      for (let i = 0; i <8; i ++) {
+  json(data: any) {
+    let currentConditions = data.currentConditions;
+    let daysData = data.days[0];
+    let hoursToDisplay = 0;
+    let AmPm = '';
+    let str: string = data.currentConditions.datetime;
+    this.houre = parseInt(str.slice(0, 2));
+    if (this.houre > 12) {
+      hoursToDisplay = this.houre - 12;
+      AmPm = 'PM';
+    } else {
+      hoursToDisplay = this.houre;
+      AmPm = 'AM';
+    }
+
+    let obj: currentConditions = {
+      hoursToDisplay: hoursToDisplay,
+      sunrise: currentConditions.sunrise,
+      sunset: currentConditions.sunset,
+      condition: currentConditions.conditions,
+      address: data.resolvedAddress,
+      temp: currentConditions.temp,
+      maxtemp: daysData.tempmax,
+      mintemp: daysData.tempmin,
+      wind: currentConditions.windspeed,
+      humidity: currentConditions.humidity,
+      pressure: currentConditions.pressure,
+      visibility: currentConditions.visibility,
+      cloud: currentConditions.cloudcover,
+      feelslike: currentConditions.feelslike,
+      icon: `../../../assets/logos/${currentConditions.icon}.png`,
+      description: daysData.descrip,
+      ampm: AmPm,
+    };
+    this.currentWeatherData = obj;
+  }
+  tempAndTime: Array<any> = [];
+  hourlyWeather = (data: any) => {
+    return new Promise((res, rej) => {
+      res(() => {});
+      let j = 1;
+      let l = 0;
+      let k = 3;
+      let forcastTime: any = 0;
+      let json = data.days;
+      let houre = this.houre;
+      let time = houre + k;
+
+      for (let i = 0; i < 8; i++) {
         if (time >= 24) {
           time -= 24;
           j++;
         }
-        if(time>12){
-          forcastTime=`${time-12} PM`;
-
-        }
-        else if(time==0){
-          forcastTime=12+' AM';
-
-        }
-        else{
-          forcastTime=time+' AM';
+        if (time > 12) {
+          forcastTime = `${time - 12} PM`;
+        } else if (time == 0) {
+          forcastTime = 12 + ' AM';
+        } else {
+          forcastTime = time + ' AM';
         }
         let obj = {
           temp: json[j].hours[time].temp,
           date: json[j].datetime,
           src: `../../../assets/logos/${json[j].hours[time].icon}.png`,
-          time:forcastTime
+          time: forcastTime,
         };
         this.tempAndTime.push(obj);
-        time+=3;
-        
-
-     }
-     
-
-  });
-  }}
+        time += 3;
+      }
+    });
+  };
+}
---
+++ src/app/dashbord/dashbord/weatherdatainterface.ts
@@ -0,0 +1,20 @@
+
+export interface currentConditions {
+  hoursToDisplay: number,
+  sunrise: string,
+  sunset: string,
+  condition: string
+  address: string,
+  temp: number,
+  maxtemp: Number,
+  mintemp: number,
+  wind: number,
+  humidity: number,
+  pressure: number
+  visibility: number,
+  cloud: number,
+  feelslike: number,
+  icon: string,
+  description: string,
+  ampm:string
+}
